7fcd84c5acee52e843bac68f9fbc73ddcaa5c9e5
averydiversifiedtech <56063439+averydiversifiedtech@users.noreply.github.com>
2019-10-24 04:02:56 -0400

    Create wazuh-ossec-discovery.pl
    Initial Commit

---
+++ scripts/wazuh-ossec-discovery.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+ 
+$first = 1;
+ 
+print "{\n";
+print "\t\"data\":[\n\n";
+ 
+for (`sudo /var/ossec/bin/agent_control -l | grep ID`)
+{
+    ($name, $status) = m/\S+ \S+ \S+ (\S+) \S+ \S+ (\S+)/;
+ 
+    print "\t,\n" if not $first;
+    $first = 0;
+$name =~ s/,\s*$//;
+ 
+    print "\t{\n";
+    print "\t\t\"{#NAME}\":\"$name\",\n";
+    print "\t\t\"{#STATUS}\":\"$status\"\n";
+    print "\t}\n";
+}
+ 
+print "\n\t]\n";
+print "}\n";
